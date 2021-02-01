d9f70d6dcc28224e9a4cb899567e97a743f5a8f7
Geonu Kim <laziu.cc@gmail.com>
2021-02-02 02:30:15 +0900

    feat: add vue support

---
+++ src/parsers/vue.ts
@@ -0,0 +1,36 @@
+import type TWClassesSorter from 'tailwind-classes-sorter'
+import prettierParserHTML from 'prettier/parser-html'
+import updateOptions from '../utils/update-options'
+
+export default (twClassesSorter: TWClassesSorter) => ({
+	...prettierParserHTML.parsers.vue,
+	parse(text, parsers, options) {
+		const ast = prettierParserHTML.parsers.vue.parse(text, parsers, options)
+
+		if (!twClassesSorter) {
+			return ast
+		}
+
+		updateOptions(twClassesSorter, options)
+
+		const cleanElementClasses = el => {
+			if (el.attrs) {
+				const classAttr = el.attrs.find(attr => attr.name === 'class')
+				if (classAttr) {
+					const classList = classAttr.value
+						.split(' ')
+						.map(classItem => classItem.trim())
+						.filter(classItem => classItem.length > 0)
+					classAttr.value = twClassesSorter.sortClasslist(classList).join(' ')
+				}
+			}
+
+			if (el.children && el.children.length > 0) {
+				el.children.forEach(childEl => cleanElementClasses(childEl))
+			}
+		}
+		cleanElementClasses(ast)
+
+		return ast
+	},
+})
